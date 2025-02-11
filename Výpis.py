c1e00a3b5cdb4b6dd2bc3395f96c05b21aefe8cd
PetrV159 <petrvoldrich12@gmail.com>
2025-02-11 15:22:12 +0100

    2 hodina

---
+++ Výpis.py
@@ -0,0 +1,10 @@
+Jmeno = "Petr"
+Prijmeni = "Voldřich"
+Vek = 17
+Vyska = 1.82
+Pohlavi = False
+
+print("Pohlavý 1 (true) - Holka, 0 (False) - Kluk")
+print(Jmeno, Prijmeni)
+print(f"Je mi", {Vek} ,"Let", "a měřím", {Vyska} ,"metrů")
+print("A pohlavý je", Pohlavi)
